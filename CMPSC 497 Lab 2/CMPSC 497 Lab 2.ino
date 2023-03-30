251d9fc940e44c87fc36ce2fc4cdc45197b0996f
Tyler Thompson <tylert123@yahoo.com>
2023-03-30 14:08:59 -0400

    Add files via upload

---
+++ CMPSC 497 Lab 2/CMPSC 497 Lab 2.ino
@@ -0,0 +1,91 @@
+/* Create a prgram so the robot can do perfect square.
+Each result will of course be different but it is very
+close each time it completes. */
+
+#define RightDirectPin1  12    //  Right Motor direction pin 1 to MODEL-X IN1 
+#define RightDirectPin2  11    // Right Motor direction pin 2 to MODEL-X IN2
+#define speedPinL 6        //  Left PWM pin connect MODEL-X ENB
+#define LeftDirectPin1  7    // Left Motor direction pin 1 to MODEL-X IN3
+#define LeftDirectPin2  8   ///Left Motor direction pin 1 to MODEL-X IN4
+
+
+/*motor control*/
+void go_Advance(void)  //Forward
+{
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,160);
+}
+
+void go_Right(int t=0)  //Turn right
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,HIGH);
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+
+void stop_Stop()    //Stop
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,LOW);
+}
+/*set motor speed */
+void set_Motorspeed(int speed_L,int speed_R)
+{
+  analogWrite(speedPinL,speed_L); 
+  analogWrite(speedPinR,speed_R);   
+}
+
+//Pins initialize
+void init_GPIO()
+{
+	pinMode(RightMotorDirPin1, OUTPUT); 
+	pinMode(RightMotorDirPin2, OUTPUT); 
+	pinMode(speedPinL, OUTPUT);  
+ 
+	pinMode(LeftMotorDirPin1, OUTPUT);
+  pinMode(LeftMotorDirPin2, OUTPUT); 
+  pinMode(speedPinR, OUTPUT); 
+	stop_Stop();
+}
+
+void setup()
+{
+	init_GPIO();
+
+	go_Advance();
+  delay(1300);
+
+  go_Right();
+  delay(540);
+
+  go_Advance();
+  delay(1300);
+
+  go_Right();
+  delay(540);
+
+  go_Advance();
+  delay(1300);
+
+  go_Right();
+  delay(540);
+
+  go_Advance();
+  delay(1300);
+  
+  stop_Stop();//Stop
+
+}
+
+void loop(){
+}
